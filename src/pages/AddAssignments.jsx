5a78e8209d9b9806c9c642c60775bf6b53d8430e
abu-tahir-sk <abutahir2626@gmail.com>
2025-12-23 11:40:56 -1000

    added my submit

--- src/pages/AddAssignments.jsx
+++ src/pages/AddAssignments.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { useNavigate } from "react-router";
--- src/pages/Pending/ErrorPage.jsx
+++ src/pages/Pending/ErrorPage.jsx
@@ -2,8 +2,9 @@
 
 const ErrorPage = () => {
       return (
-            <div>
-                  <h4>Page note fount</h4>
+            <div className="flex bg-white dark:bg-gray-800 flex-col justify-center items-center font-extrabold min-h-screen">
+                  <h1 className="text-[156px]">404</h1>
+                  <h4 className="text-5xl ">Page note fount</h4>
             </div>
       );
 };
